68836025784a6d018351cabe55a62743501e2ce9
Pooja-S-2006 <poojasenthil@1521>
2026-04-18 20:35:16 +0530

    Initial commit: Main application

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./ui-components/src/**/*.{js,ts,jsx,tsx}",
+    "./product-module/src/**/*.{js,ts,jsx,tsx}",
+    "./cart-module/src/**/*.{js,ts,jsx,tsx}",
+    "./auth-module/src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
